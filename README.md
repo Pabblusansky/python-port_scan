e7e2197488b3441ed21c1c1ca95a00892cf190f5
Pabblusansky <pchirtoca@gmail.com>
2024-10-14 13:43:04 +0300

    Ctrl+C Interrupt Implementation + SYN scan (1.3)

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Python Port Scanner
 Python Simple Port Scanner
-Port Scanner is a simple and efficient port scanning application written in Python using the powerful Scapy library. It allows users to quickly check port availability on target IP addresses.
+Port Scanner is a simple and efficient thread-based port scanning application written in Python using the powerful Scapy library. It allows users to quickly check port availability on target IP addresses.
 # Dependencies
 1.Scapy<br/>
 2.npcap
 # Functions
-Scanning one or more ports on a given valid IP address.<br/>
+Scanning one or all ports on a given valid IP address.<br/>
 Detect open and closed ports and display information about each port.<br/>
-Thread based port scan for a fast scanning speed<br/>
 Supports external configuration file to store port information (in JSON format).<br/>
+Thread based port scan for a fast scanning speed<br/>
+TCP Stealth(SYN) Scan of all ports from the JSON file<br/>
 Logging the results in a txt file.<br/>
 # Installation
 1.Clone the repository:
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from scapy.all import ICMP, IP, TCP, sr1
+from scapy.all import ICMP, IP, TCP, sr1, send
 import sys
-import socket
+#import socket
 from datetime import datetime
 import json
 import logging
@@ -16,6 +16,8 @@ logging.basicConfig(
 )
 #Open Ports array
 open_ports = []
+#Stop scan threading event
+stop_scan = threading.Event()
 # Lock for thread-safe access to open_ports
 open_ports_lock = threading.Lock()
 # Function to load ports from JSON file
@@ -25,45 +27,66 @@ def load_ports(filename):
     return {int(key): value for key, value in ports_dict.items()}
 #Loading JSON 
 ports = load_ports('ports.json')
-#Scan port function
-def scan_port(ip, port): 
-    syn_packet = IP(dst=ip)/TCP(dport=port, flags="S")
-    response = sr1(syn_packet, timeout=1, verbose=0)
-    if response:
-        if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
-            print(f"Port {port} ({ports.get(port, 'Unknown')}) is open")
-            open_ports.append(port)
-        elif response.haslayer(TCP) and response.getlayer(TCP).flags == 0x14:  # RST+ACK
-            print(f"Port {port} ({ports.get(port, 'Unknown')}) is closed")
-    else:
-        print(f"Port {port} ({ports.get(port, 'Unknown')}) is filtered or no response")
-    logging.info(f"Scanned port {port}: {'open' if port in open_ports else 'closed'}") # Logging the results
-#Scan ports from a list of ports function
-def scan_ports(ip):
+#Scan port function (TCP/Stealth)
+def scan_port(ip, port, stealth=False):
+    if stop_scan.is_set():
+        return False
     try:
-        logging.info(f"\n=== Starting new scan on {ip} ===")
-        print(f"Starting scan on {ip}")
-        start_time = datetime.now()
+        syn_packet = IP(dst=ip)/TCP(dport=port, flags="S")
+        response = sr1(syn_packet, timeout=1, verbose=0)
+        if response:
+            if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+                with open_ports_lock:
+                    print(f"Port {port} ({ports.get(port, 'Unknown')}) is open")
+                    open_ports.append(port)
+                if stealth:
+                    rst_packet = IP(dst=ip) / TCP(dport=port, flags="R")
+                    send(rst_packet, verbose=0)
+            elif response.haslayer(TCP) and response.getlayer(TCP).flags == 0x14:  # RST+ACK
+                print(f"Port {port} ({ports.get(port, 'Unknown')}) is closed")
+        else:
+            print(f"Port {port} ({ports.get(port, 'Unknown')}) is filtered or no response")
+        logging.info(f"Scanned port {port}: {'open' if port in open_ports else 'closed'}") # Logging the results
+    except KeyboardInterrupt:
+        stop_scan.set()
+        raise
+    except Exception as e:
+        print(f"Error occurred while scanning port {port}: {e}")
+#Scan ports from a list of ports function
+def scan_ports(ip, stealth=False):
+    global open_ports
+    open_ports = []
+    stop_scan.clear()
+    logging.info(f"\n=== Starting new scan on {ip} ===")
+    print(f"Starting scan on {ip}")
+    start_time = datetime.now()
+    try:
         # Thread scanning ports
         threads = []
         for port in ports:
-            thread = threading.Thread(target=scan_port(ip, port), args=(ip, port))
+            if stop_scan.is_set():
+                break
+            thread = threading.Thread(target=scan_port, args=(ip, port, stealth))
             threads.append(thread)
             thread.start()
         for thread in threads:
             thread.join()
-        
-        end_time = datetime.now()
-        print(f"Scan finished in {end_time - start_time}")
-        print(f"Scanned {len(ports)} ports: {len(open_ports)} open, {len(ports) - len(open_ports)} closed")
-        if open_ports:
-            print("\nOpen ports:")
-            for port in open_ports:
-                print(f"{port} ({ports.get(port, 'Unknown')})")
-        else:
-            print("No open ports found.")
+        if not stop_scan.is_set():
+            end_time = datetime.now()
+            print(f"Scan finished in {end_time - start_time}")
+            print(f"Scanned {len(ports)} ports: {len(open_ports)} open, {len(ports) - len(open_ports)} closed")
+            if open_ports:
+                print("\nOpen ports:")
+                for port in open_ports:
+                    print(f"{port} ({ports.get(port, 'Unknown')})")
+            else:
+                print("No open ports found.")
+    except KeyboardInterrupt:
+        stop_scan.set()
+        print(f"\nScan aborted.")
     except Exception as e:
         print(f"Error occured: {e}")
+        logging.error(f"Error occurred: {e}")
 # Scan specific ports function
 def scan_specific_port(ip, port):
     logging.info(f"\n=== Starting new scan on {ip} ===")
@@ -86,9 +109,10 @@ def main_menu():
         # Using get ip function
         target_ip = get_target_ip()
         print("\nChoose an action:")
-        print("1. Scan a specific port")
-        print("2. Scan all common ports")
-        print("3. Exit")
+        print("1. Scan a specific port(TCP)")
+        print("2. Scan all common ports(TCP)")
+        print("3.Scan all commmon ports(TCP Stealth/SYN scan)")
+        print("4. Exit")
         
         choice = input("Enter your choice (1/2/3): ").strip()
         if choice == '1':
@@ -104,8 +128,10 @@ def main_menu():
                     print("Please enter a valid number.")
 
         elif choice == '2':
-            scan_ports(target_ip)
+            scan_ports(target_ip, stealth=False)
         elif choice == '3':
+            scan_ports(target_ip, stealth=True)
+        elif choice == '4':
             print("Exiting...")
             break  
         else:
